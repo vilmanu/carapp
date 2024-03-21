61afb03f68dd3c91aefada262b0a0de2359defbb
vilmanu <manuel.ssorio3@gmail.com>
2024-03-21 08:43:35 -0300

    app redy

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # carapp
+app para ver en graficos los distintas estadisticas de autos con histogramas y tambien con graficos de dispersion
--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
+
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
+build_histogram = st.checkbox('Construir un histograma')
+hist_button = st.button('Construir histograma') # crear un botón  
+
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')    
+    if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+        fig = px.histogram(car_data, x="odometer")
         
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
-        
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        st.plotly_chart(fig, use_container_width=True)
+    
+scatter_button = st.button('Construir grafico de dispersion') # crear un botón
+if scatter_button: # al hacer clic en el botón
+    # ecribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
     fig = px.scatter(car_data, x="odometer")
