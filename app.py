160e41a3fe41d259710223f991670c0c802e1e17
vilmanu <manuel.ssorio3@gmail.com>
2024-03-21 09:38:05 -0300

    new

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.header("Crea un grafico")
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 build_histogram = st.checkbox('Construir un histograma')
@@ -15,7 +16,7 @@ if build_histogram: # si la casilla de verificación está seleccionada
         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
-        fig = px.histogram(car_data, x="odometer")
+        fig = px.histogram(car_data, x="odometer", y="price")
         
     # mostrar un gráfico Plotly interactivo
         st.plotly_chart(fig, use_container_width=True)
